4c1d5cc72d8b3efc434e11a17eda1437fe981e71
Christophe Porteneuve <tdd@tddsworld.com>
2015-02-11 14:42:57 +0100

    refactor(js): split init phases in separates functions.

--- js/playground.js
+++ js/playground.js
@@ -2,6 +2,11 @@
   document.addEventListener('DOMContentLoaded', initPlayground);
 
   function initPlayground() {
+    decorateRefLangs();
+    // Add further init functions here
+  }
+
+  function decorateRefLangs() {
     var items = document.querySelector('*[hreflang]');
     items = Array.prototype.slice.call(items);
     items.forEach(function(item) {
