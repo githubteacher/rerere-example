06b137b0cb096671a06c3fa46841c46b9af88e3f
Christophe Porteneuve <tdd@tddsworld.com>
2015-02-11 12:44:11 +0100

    fix(js): Make initPlayground usable using an FD instead of an FE. Fixes #42.

--- js/playground.js
+++ js/playground.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', initPlayground);
 
-initPlayground = function() {
+function initPlayground() {
   is = document.querySelector('*[hreflang]');
   is.forEach(function(i) {
     html = ' <span class="langInfo">(' + i.getAttribute('hreflang') + ')</span>';
