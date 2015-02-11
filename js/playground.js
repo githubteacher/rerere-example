0a048907f07488b446141cbcbfb6acba73514a94
Christophe Porteneuve <tdd@tddsworld.com>
2015-02-11 14:47:45 +0100

    fix(js): Use ES6's  to ensure  is an actual Array.

--- js/playground.js
+++ js/playground.js
@@ -3,7 +3,7 @@
 
   function initPlayground() {
     var items = document.querySelector('*[hreflang]');
-    items.forEach(function(item) {
+    Array.from(items).forEach(function(item) {
       var html = ' <span class="langInfo">(' + item.getAttribute('hreflang') + ')</span>';
       item.insertAdjacentHTML('beforeend', html);
     });
