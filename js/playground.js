0ad02588e5a7f5111ced513c36b00dc1a584a335
Christophe Porteneuve <tdd@tddsworld.com>
2015-02-11 14:41:44 +0100

    fix(js): Make  an actual array instead of a NodeList, so  works.  Fixes #48.

--- js/playground.js
+++ js/playground.js
@@ -3,6 +3,7 @@
 
   function initPlayground() {
     var items = document.querySelector('*[hreflang]');
+    items = Array.prototype.slice.call(items);
     items.forEach(function(item) {
       var html = ' <span class="langInfo">(' + item.getAttribute('hreflang') + ')</span>';
       item.insertAdjacentHTML('beforeend', html);
